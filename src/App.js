1ecbe1c3a75a3b1f608825f2ecc970673f78de18
DahianaDuque <dahiana.duque567@pascualbravo.edu.co>
2024-11-24 23:03:29 -0500

    Imagenes

--- src/App.js
+++ src/App.js
@@ -34,6 +34,12 @@ function App() {
       </section>
 
       <section id="about" className="section about">
+        <div className="image-container">
+        <img src={require('./Imagenes/1.jpg')} alt="Imagen 1" />
+        <img src={require('./Imagenes/2.jpg')} alt="Imagen 2" />
+        <img src={require('./Imagenes/3.jpg')} alt="Imagen 3" />
+        <img src={require('./Imagenes/4.jpg')} alt="Imagen 4" />
+        </div>
         <h1>Sobre mí</h1>
         <p>Soy un desarrolladora apasionada por los gatos y por intentar crear un mundo mas dinamico y divertido a la hora de programar.</p>
         <p>Me encanta resolver problemas y aprender nuevas tecnologías.</p>
